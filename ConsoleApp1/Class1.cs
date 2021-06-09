0c49e1fa1d583ffeff19e67b50c667a5e93e0f4a
Chingis B. Sanzhiev <cbs1@tpu.ru>
2021-06-09 16:33:20 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class student
+    {
+        public string name;
+        public int score;
+        public void Itog()
+        {
+            Console.WriteLine($"Loh {name}, u tebya {score}");
+        }
+
+
+        public student(string name)
+        {
+            this.name = name;
+        }
+
+        public student(string n, int s) : this(n)
+        {
+
+            score = s;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            student bob = new student("Abay",3);
+            bob.Itog();
+
+            student tom = new student("Leha") { score = 5 };
+            tom.Itog();
+
+
+            user j = new user("Vlad",4);
+           
+            j.Itog();
+
+            
+        }
+    }
+
+    struct user
+    {
+        public string name;
+        public int score;
+        public user(string name,int score)
+        {
+            this.name = name;
+            this.score = score;
+        }
+        public void Itog()
+        {
+            Console.WriteLine($"Loh {name}, u tebya {score}");
+        }
+
+
+        
+    }
+}
